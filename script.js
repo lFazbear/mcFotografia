d5ab85e79cd897e8ab78e55079f9b6e33474b614
EDGAR <109559189+lFazbear@users.noreply.github.com>
2026-03-10 12:19:04 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const toggleBtn = document.getElementById('theme-toggle');
+const body = document.documentElement;
+
+toggleBtn.addEventListener('click', () => {
+    if (body.getAttribute('data-theme') === 'dark') {
+        body.removeAttribute('data-theme');
+        localStorage.setItem('theme', 'light');
+    } else {
+        body.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+});
+
+// Guardar preferencia del usuario
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    body.setAttribute('data-theme', savedTheme);
+}
